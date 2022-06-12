3a30dce507b3e6ba0ea5e238573bae15db270901
ANUPAM KHOSLA <anupam.khosla@oxideinteractive.com.au>
2022-06-12 20:39:18 +1000

    New branch for rtl css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
 const gulp = require('gulp');
 const webp = require('gulp-webp');
- 
-gulp.task('default', () =>
+var rtlcss = require('rtlcss');
+var fs = require("fs");
+var browserSync = require('browser-sync').create();
+
+
+
+gulp.task('rtl', function(cb) { 
+    var fileContent = fs.readFileSync("./css/style.css", "utf8");
+    var result = rtlcss.process( fileContent );
+    fs.writeFileSync('./css/style_rtl.css', result);    
+    cb();
+});
+
+
+
+gulp.task('browser-sync', function() {    
     gulp.src('./img/*')
         .pipe(webp())
-        .pipe(gulp.dest('dist'))
-);
+        .pipe(gulp.dest('dist'));
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
